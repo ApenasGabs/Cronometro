7b45156513a0fc41d2d8ddf3c86a49c3b6c07b3d
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-01-04 16:20:46 -0300

    :fire:

---
+++ cronometro.js
@@ -0,0 +1,24 @@
+"use strict"
+
+
+let hora = 0;
+let minutos = 0;
+let segundos = 0;
+let milisegundos = 0;
+
+let tempo = 0;
+let cronometro;
+
+
+function comecar() {
+
+}
+function parar() {
+    
+}
+function limpar() {
+    
+}
+function start() {
+    
+}
